f18bf87c09f0ca1cdd903a386f7c70441a262d7a
Igor Yermak <igor.yermak@gmail.com>
2018-02-08 22:16:00 -0500

    Nav icons and put boxes under main class

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Icon from './components/icon.js';
+import './components/icon.css';
+import home from './home.png';
+import jobs from './jobs.png';
+import network from './network.png';
+import messaging from './messaging.png';
+import notifications from './notifications.png';
+import user from './user.png';
 import LeftRail from './components/LeftRail.js';
 import CenterRail from './components/CenterRail.js';
 import RightRail from './components/RightRail';
@@ -8,18 +16,32 @@ import RightRail from './components/RightRail';
 class App extends Component {
   render() {
     return (
-      <div className="Nav">
-        <header className="App-header">
-          <h1 className="App-title">Linked</h1>
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <ul className="Nav">
+          <div className="App-header">
+            <h1 className="App-title">Linked</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="end">
 
+            <input type="text" placeholder="Search"></input>
+          </div>
+          <div className="icons">
+            <li><Icon source={home} alt="Home" /></li>
+            <li><Icon source={network} alt="Network" /></li>
+            <li><Icon source={jobs} alt="Jobs" /></li>
+            <li><Icon source={messaging}alt="Messaging"/></li>
+            <li><Icon source={notifications} alt="Notifications" /></li>
+            <li><Icon source={user} alt="User" /></li>
+          </div>
 
-          <p className="App-intro">
-
-          </p>
-        </header><input type="text" placeholder="Search"></input> <LeftRail />
+        </ul>
+        <div className="Rails">
+        <LeftRail />
         <CenterRail />
         <RightRail />
+
+      </div>
       </div>
     );
   }
